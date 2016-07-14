8331f626bb28b41a84b99908df50adb730327a1f
Radek Benes <radekbenes@centrum.cz>
2016-07-14 13:24:39 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import {FIREBASE_PROVIDERS, defaultFirebase} from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [FIREBASE_PROVIDERS, defaultFirebase({
+    apiKey: "AIzaSyCixFZpJ-rcW1mXHAeMWyeMNK7J5adOcdc",
+    authDomain: "radeg-businesscontacts.firebaseapp.com",
+    databaseURL: "https://radeg-businesscontacts.firebaseio.com",
+    storageBucket: "radeg-businesscontacts.appspot.com",
+  })]);
 
