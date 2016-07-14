59b569fca872d281be8144f99a2e92615e38fd5e
Radeg <radekbenes@centrum.cz>
2016-07-14 17:41:51 +0200

    14-07-16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import {BusinessService} from './services/business.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [BusinessService]
 })
-export class AppComponent {
-  title = 'app works!';
-  items: FirebaseListObservable<any>;
 
-  constructor(_af: AngularFire) {
-    this.items = _af.database.list('/items');
+export class AppComponent implements OnInit {
+  title = 'app works!';
+  businesses: Business[];
+  categories: Category[];
+
+  constructor(private _businessService: BusinessService) {
+
   }
+
+  ngOnInit() {
+    this._businessService.getBusinesses().subscribe(businesses => {
+      this.businesses = businesses
+    })
+
+    this._businessService.getCategories().subscribe(categories => {
+      this.categories = categories
+    })
+  }
+}
+
+export interface Business {
+  $key?: string;
+  company?: string;
+  description?: string;
+  category?: string;
+  years_in_business?: string;
+  street_address?: string;
+  city?: string;
+  state?: string;
+  zipcode?: string;
+  phone?: string;
+  email?: string;
+  created_at?: string;
+}
+
+export interface Category {
+  $key?: string;
+  name?: string;
 }
---
+++ src/app/services/business.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class BusinessService {
+  businesses: FirebaseListObservable<Business[]>;
+  categories: FirebaseListObservable<Category[]>;
+
+  constructor(private _af: AngularFire) {
+  }
+
+  getBusinesses() {
+    this.businesses = this._af.database.list('/businesses') as FirebaseListObservable<Business[]>;
+    return this.businesses;
+  }
+
+  getCategories() {
+    this.categories = this._af.database.list('/categories') as FirebaseListObservable<Category[]>;
+    return this.categories;
+  }
+
+}
+
+export interface Business {
+  $key?: string;
+  company?: string;
+  description?: string;
+  category?: string;
+  years_in_business?: string;
+  street_address?: string;
+  city?: string;
+  state?: string;
+  zipcode?: string;
+  phone?: string;
+  email?: string;
+  created_at?: string;
+}
+
+export interface Category {
+  $key?: string;
+  name?: string;
+}
